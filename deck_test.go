bd2544eeb9fdb46fb2983a751a7baa310fb63346
nandrease <neeme.andreas.eller@gmail.com>
2020-03-16 17:25:50 +0200

    Test reading and writing files

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -15,5 +18,18 @@ func TestNewDeck(t *testing.T) {
 
 	if d[len(d)-1] != "King of Clubs" {
 		t.Errorf("Expected first card to be King of Clubs, but got %v", d[0])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards in deck, got %v", len(loadedDeck))
 	}
 }
