cdf97f6ab9724c88edb5a8367323e1c36f5d8aa5
nandrease <neeme.andreas.eller@gmail.com>
2020-03-16 15:45:20 +0200

    Add newDeck() function to generate cards

--- deck.go
+++ deck.go
@@ -7,8 +7,23 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Heart", "Club"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (s deck) print() {
-	for i, card := range s {
-		fmt.Println(i, card)
+	for _, card := range s {
+		fmt.Println(card)
 	}
 }
--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
 func main() {
-	cards := deck{newCard(), "Ace", "King"}
-	cards = append(cards, "Queen", "Jack")
+	cards := newDeck()
 
 	cards.print()
-}
-
-func newCard() string {
-	return "Joker"
 }
